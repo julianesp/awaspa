3c5c4606ee58f8cd1f318189d7cebd7cf748a169
Julián España <julii1295@gmail.com>
2022-03-31 15:11:06 -0500

    Archivos subidos

---
+++ js/links.js
@@ -0,0 +1,35 @@
+function estadosFinancieros(params) {
+    location.href = '../assets/documents/1. ESTADOS FINANCIEROS 2021.pdf'
+}
+
+function requisitos(params) {
+    location.href = '../assets/documents/2. Certificado de requisitos del representante Legal FINAL.pdf'
+}
+
+function documentoLegal(params) {
+    location.href = '../assets/documents/3. Documento Legal de Constitución.pdf'
+}
+
+function existencia(params) {
+    location.href = '../assets/documents/4. Certificado de existencia y representación Legal. FINAL.pdf'
+}
+
+function acta(params) {
+    location.href = '../assets/documents/5. Acta de Asamblea General.pdf'
+}
+
+function estatutos(params) {
+    location.href = '../assets/documents/6. ESTATUTOS.pdf'
+}
+
+function representante(params) {
+    location.href = '../assets/documents/7. Certificación Representante legal antecedentes judiciales.pdf'
+}
+
+function remuneracion(params) {
+    location.href = '../assets/documents/8. Certificación remuneración Junta Directiva y Gerencia FINAL.pdf'
+}
+
+function principal(params) {
+    location.href = '../index.html'
+}
